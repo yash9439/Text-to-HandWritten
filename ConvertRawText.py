09dec3fbaa83d1ecb7862360884521d9ae84928b
suyashsethia <suyash.sethia0848@gmail.com>
2024-03-12 16:04:10 +0530

    code updated to take random letter from the datasets available (currently 2 )

--- ConvertRawText.py
+++ ConvertRawText.py
@@ -1,7 +1,12 @@
 from PIL import Image
 import string
+import random
 
-back = Image.open("zback.png")
+
+#random word either 1 or 0 
+
+
+back = Image.open("Dataset_online/zback.png")
 width,height = 50,0
 arr = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 arr = arr+" "
@@ -9,23 +14,30 @@ arr = arr+" "
 def condn(case):
     global width,height
     print(case)
-    cases = Image.open("%s.png"%case)
+    k = random.randint(0,1)
+    folder=""
+    if(k==0):
+        folder = "Dataset_online/"
+    elif(k==1):
+        folder = "letters_white_seperated (3rd copy)/"
+    path = folder+case+".png"
+    cases = Image.open(path)
     back.paste(cases,(width,height))
     newwidth = cases.width
     width = width+newwidth
 
-inp = "Ekdum krishna lage che WOW , but how ? "
+inp = "Chandu ke Chacha ne chandu ki chachi ko chandni raat me chatni chatai "
 
 for letter in inp:
     if letter in arr:
         if letter == " ":
             letter = "zspace"
         if letter ==".":
-            letter = "fs"
+            letter = "fs_"
         if letter == ",":
-            letter = "coma"
+            letter = "coma_"
         if letter == "?":
-            letter = "que"
+            letter = "que_"
         condn(letter)
         
 back.show()
