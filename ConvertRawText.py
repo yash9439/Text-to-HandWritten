7110ceded7e5fed03d3499bec971c7fa81cb98bf
suyashsethia <suyash.sethia0848@gmail.com>
2024-03-12 16:32:36 +0530

    final code updated

--- ConvertRawText.py
+++ ConvertRawText.py
@@ -4,7 +4,7 @@ import random
 
 
 #random word either 1 or 0 
-
+used_width , used_height = 0,0
 
 back = Image.open("Dataset_online/zback.png")
 width,height = 50,0
@@ -13,6 +13,7 @@ arr = arr+" "
 
 def condn(case):
     global width,height
+    # global used_width , used_height
     print(case)
     k = random.randint(0,1)
     folder=""
@@ -22,11 +23,16 @@ def condn(case):
         folder = "letters_white_seperated (3rd copy)/"
     path = folder+case+".png"
     cases = Image.open(path)
+    letter_width = cases.width
+    
     back.paste(cases,(width,height))
     newwidth = cases.width
     width = width+newwidth
+    if (width > 6000):
+        width = 50
+        height = height + 300
 
-inp = "Chandu ke Chacha ne chandu ki chachi ko chandni raat me chatni chatai "
+inp = "Virat Kohli is a world-famous Indian cricketer who has given multiple chances to the whole country to feel proud because of his personality and achievements. Writing an essay on Virat Kohli in English is fun and interesting because there are so many interesting facts about this cricketer, not just as a sportsman but as a person too. Virat Kohli is an inspiring figure and a role model for many youngsters and children. He has brought a class to Indian cricket. Under his captaincy, the Indian cricket team has reached heights. The legacy of Virat Kohli will never end, so this topic for essay writing will remain for long."
 
 for letter in inp:
     if letter in arr:
@@ -40,4 +46,6 @@ for letter in inp:
             letter = "que_"
         condn(letter)
         
+#save the image
+back.save("Final_output.png")
 back.show()
